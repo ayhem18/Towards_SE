4e2b30e90f32b95c9a399386fc3ecdcc0d91729e
ayhem bouabid <bouabidayhem@gmail.com>
2024-08-09 06:16:28 +0300

    adding notes from the flexbox layout ytb video

--- ProblemSolving/Python/hashing/test_hash.py
+++ ProblemSolving/Python/hashing/test_hash.py
@@ -13,9 +13,9 @@ def generate_random_string(str_len: int) -> str:
     return "".join([chr(ord('a') + random.randint(0, 25)) for _ in range(str_len)])
 
 def test_first_not_repeat_char(num_tests:int = 10 ** 5):
-    n = random.randint(1, 1000)
-    string = generate_random_string(str_len=n)    
     for _ in tqdm(range(num_tests), desc='testing !!!'):
+        n = random.randint(1, 1000)
+        string = generate_random_string(str_len=n)    
         o1, o2 = first_not_repeat_char_1_traversal(string), first_not_repeat_char_2_traversals(string)
         assert o1 == o2, f"The code breaks for the string {string}"
 
