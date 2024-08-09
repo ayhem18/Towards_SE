06990ed7fb2bb62e27b47098f7b176616aff0bc2
ayhem bouabid <bouabidayhem@gmail.com>
2024-08-10 01:37:35 +0300

    an even more interactive shopping list

--- web_dev/frontend/JavaScript/DOM/basic_shopping/script.js
+++ web_dev/frontend/JavaScript/DOM/basic_shopping/script.js
@@ -1,3 +1,5 @@
+// making the list interactive !!!
+
 let items_list = []; // an array to save the name of the list 
 
 // main_section = document.querySelector("main");
@@ -5,8 +7,7 @@ let items_list = []; // an array to save the name of the list
 // create the list element
 const items_list_node = document.createElement("ul");
 
-// styling the element with the "shop_list" class style...
-// items_list_node.classList.add("shop_list"); (not necessarily apparently)
+const reset_footer = document.getElementsByClassName("reset_footer")[0]
 
 // create a function to add the elements to the list
 function add_item(event) {
@@ -47,7 +48,9 @@ function add_item(event) {
     // list_item.innerText = item_name;
 
     if (items_list.length == 1) {
-        document.body.appendChild(items_list_node);
+        document.body.insertBefore(items_list_node, reset_footer)
+        // the line of code below would have worked if we did not have the extra footer
+        // document.body.appendChild(items_list_node);
     }
 
     items_list_node.appendChild(list_item);
@@ -67,9 +70,34 @@ function delete_item(event) {
     // find the button
     let list_item = event.target.parentElement;
     // the parent element should be the <li> element in the shopping list
-    // remove it from the DOM as well as the list
+    // removing the <li> element will remove everything from the shopping list
     list_item.remove();
-    items_list.remove(list_item);
+    // items_list.remove(list_item);
 }
 
 items_list_node.addEventListener("click", delete_item);
+
+// let's use the html text to modify everything
+function reset () {
+    // if there is a list of any kind complete remove it
+    shopping_lists = document.getElementsByTagName("ul")
+    if (shopping_lists.length == 0) {
+        console.log("Nothing to reset")
+        return;
+    }
+
+    // remove all children with no exception
+    shopping_lists[0].replaceChildren();
+
+    // innerHTML attribute is scary...
+    // shopping_lists[0].innerHTML = "<li> a new item out of nowhere</li>";
+
+    // remove the shopping list from the DOM
+    shopping_lists[0].remove();
+
+    // make sure to empty the items_list as well
+    items_list = []
+}
+
+const reset_button = document.getElementById("reset_button");
+reset_button.addEventListener("click", reset);
