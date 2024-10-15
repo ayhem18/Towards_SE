61cf728714df340fdfd10689035a6808b939ada0
ayhem bouabid <bouabidayhem@gmail.com>
2024-10-15 12:53:47 +0300

    getting the basics of request in spring boot:

--- backend_projects/java/quizz_app/src/main/java/com/example/quizz_app/Car.java
+++ backend_projects/java/quizz_app/src/main/java/com/example/quizz_app/Car.java
@@ -3,11 +3,14 @@ package com.example.quizz_app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Car {
+    private static int CAR_COUNT = 0;
     private final String name;
     private final int year;
-
+    private int car_id; // keeps track for all the car objects that were created in the program so far...
 
     public Car(String n, int y) {
+        Car.CAR_COUNT += 1;
+        this.car_id = Car.CAR_COUNT;
         this.name = n;
         this.year = y;
     }
@@ -19,6 +22,10 @@ public class Car {
 
     public int getYear() {
         return year;
+    }
+
+    public int getCar_id() {
+        return car_id;
     }
 }
 
--- backend_projects/java/quizz_app/src/main/java/com/example/quizz_app/QuizzAppApplication.java
+++ backend_projects/java/quizz_app/src/main/java/com/example/quizz_app/QuizzAppApplication.java
@@ -4,36 +4,68 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
 
-import com.example.quizz_app.Car;
 
 @SpringBootApplication
 @RestController
 public class QuizzAppApplication {
 
+	// using a thread safe collection such as ConcurrentHashMap
+	private final ConcurrentHashMap<Integer, Car> carMap = new ConcurrentHashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzAppApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue="World") String name) {
-		return "mind your own business";
+
+//	 @PostMapping("/car")
+//	 public String carPostEndpoint(@RequestParam(value = "name") String name,
+//							   @RequestParam(value = "year") int year) {
+//		 Car c = new Car(name, year);
+//		 this.carMap.put(c.getCar_id(), c);
+//		 return "Car added successfully";
+//	 }
+
+
+	@PostMapping("/car")
+	public String carPostEndpointBody(@RequestBody Car car) {
+		if (this.carMap.containsKey(car.getCar_id())) {
+			return "There is already a car with id " + car.getCar_id();
+		}
+		this.carMap.put(car.getCar_id(), car);
+		return "The car is added successfully";
 	}
 
-	 @GetMapping("/car")
-	 public String carEndpoint(@RequestParam(value = "name", defaultValue="Jaguar") String name,
-	 						  @RequestParam(value = "year", defaultValue="2002") int year) {
 
-		Car c = new Car(name, year);
+	@GetMapping("/car/{car_id}/")
+	public String carGetEndpointPath(@PathVariable int car_id) {
 		ObjectMapper obj = new ObjectMapper();
 		try {
-			return obj.writeValueAsString(c);
-		}catch (Exception e) {
+			Car c = this.carMap.get(car_id);
+			if (c == null) {
+				throw new NoSuchElementException();
+			}
+			return obj.writerWithDefaultPrettyPrinter().writeValueAsString(this.carMap.get(car_id));
+		}
+		catch (NoSuchElementException e) {
+			return "there is no car with id " + car_id + " saved in the database";
+		}
+
+		catch (Exception e) {
 			return "Some error happened";
 		}
-	 }
+	}
+
+	// you can also use path variables
+	@GetMapping("/car")
+	public List<Car> allCarsEndpoint(){
+		return new ArrayList<>(this.carMap.values());
+	}
+
 
 }
