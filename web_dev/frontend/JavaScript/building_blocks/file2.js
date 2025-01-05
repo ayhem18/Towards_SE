3573b053dc91cb9bba000062ebd065b309663879
ayhem18 <bouabidayhem@gmail.com>
2025-01-06 00:26:50 +0300

    js session 05.01.2025

--- web_dev/frontend/JavaScript/building_blocks/file2.js
+++ web_dev/frontend/JavaScript/building_blocks/file2.js
@@ -1,18 +1,3 @@
-// add a listener to the container 
-
-// const container_element = document.querySelector(". container");
-
-// function chance_text_color(event) {
-//     // extract the target
-//     let target_element = event.target
-//     let color = target_element.getAttribute("data-color")
-//     target_element.style.color = color;
-// }
-
-
-// container_element.addEventListener("click", chance_text_color)
-
-
 // this is a script to run few lines of code
 function checkTheDate(value){
     switch ((1 <= value) && (value <= 5)) {
@@ -31,3 +16,51 @@ function showPlanets(planets) {
 }
 
 showPlanets(planets);
+
+let firstStr = "Apple";
+let secondStr = "Apple Iphone";
+
+let compareTwo = secondStr.localeCompare(firstStr)
+console.log(compareTwo);
+
+
+let txt = "I slit the sheet, the sheet I slit, and on the slitted sheet I sit";
+let searchStr = "sheet";
+
+console.log(txt.search(searchStr));
+
+function prepareData(line) {
+    return line.trim().toLowerCase();
+    //write your code here
+}
+
+console.log(prepareData("    Live has always been shit little mna..        "));
+
+
+function speak(n) {
+    let temp = "bark ".repeat(n)
+    return temp.substring(0, temp.length - 1);
+    //write your code here
+}
+
+let t = speak(3);
+console.log(t.length);
+
+function introduction(line) {
+    let parts = line.split(" ")
+    return parts[parts.length - 1];
+}
+
+
+console.log(introduction("Hello I am Ayhem"));
+console.log(introduction("Hello I am Khalifa"));
+
+
+function greeting(line) {
+    let name = line.substring(0, 10).trim()
+    let job = line.substring(21, line.length).trim();
+    //write your code here
+    return ("My name is " + name + " and I'm a " + job + ".");
+}
+
+console.log(greeting("Ayhemovich123456789  ML engineer"));
