941d6be7c087661c060534b7e28611674deada74
ayhem18 <bouabidayhem@gmail.com>
2025-01-17 16:55:56 +0300

    java spring deeper dive

---
+++ SpringAcademy/restaurant_reward_network/src/test/java/com/restaurant_reward_network/RestaurantRewardNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant_reward_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRewardNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
